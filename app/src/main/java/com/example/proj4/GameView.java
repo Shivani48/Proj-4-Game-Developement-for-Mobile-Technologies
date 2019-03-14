8f95a36cdc8021f88feca7b2be554989c753d1a8
Shivani Dhanasamy <shivanidhanasamy04@gmail.com>
2019-03-14 19:35:11 -0400

    try 2

---
+++ app/src/main/java/com/example/proj4/GameView.java
@@ -0,0 +1,36 @@
+package com.example.proj4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameView extends AppCompatActivity {
+
+    private ImageButton buttonPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setting the orientation to landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //getting the button
+        buttonPlay = (ImageButton) findViewById(R.id.buttonPlay);
+
+        //adding a click listener
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //starting game activity
+        startActivity(new Intent(this, GameActivity.class));
+    }
+}
+
+
+
+}
+
